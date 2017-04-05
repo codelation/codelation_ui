ecee8b421bca6b8917758ea25224fbc726081a49
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-05 11:56:28 -0500

    Update codelation_ui.js

--- app/assets/javascripts/codelation_ui.js
+++ app/assets/javascripts/codelation_ui.js
@@ -1,1 +1,6 @@
-//= require ./codelation_ui/first
+//= require ./codelation_ui/base
+//= require ./codelation_ui/vue
+//= require ./codelation_ui/std
+//= require ./codelation_ui/forms
+//= require ./codelation_ui/extended
+//= require ./codelation_ui/vue_pages
