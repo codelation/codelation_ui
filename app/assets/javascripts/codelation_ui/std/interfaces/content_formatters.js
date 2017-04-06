e6325a8b844dc984ea104a43ee7bb8a0e54f0312
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-06 17:40:52 -0500

    Update content_formatters.js

--- app/assets/javascripts/codelation_ui/std/interfaces/content_formatters.js
+++ app/assets/javascripts/codelation_ui/std/interfaces/content_formatters.js
@@ -1,28 +1,38 @@
 (function() {
   "use strict";
+  
+  // Config options
+  App.vue.config.contentFormatters = {
+    'empty': '--',
+    'currency': '0,0[.]00',
+    'percent': '0,0[.]00',
+    'number': '0,0[.]00',
+    'date': 'MM/DD/YYYY',
+  };
+  
   App.vue.interfaces.contentFormatters = {
     methods: {
       _formatToCurrency: function(value, custom_number_format, nullReturn) {
         if (value === undefined || value === null || isNaN(value)) {
-         return nullReturn || '-'; 
+         return nullReturn || App.vue.config.contentFormatters.empty; 
         }
-        return numeral(value).format('$' + (custom_number_format || '0,0[.]00'));
+        return numeral(value).format('$' + (custom_number_format || App.vue.config.contentFormatters.currency));
       },
       _formatToPercent: function(value, custom_number_format, nullReturn) {
         if (value === undefined || value === null || isNaN(value)) {
-         return nullReturn || '-'; 
+         return nullReturn || App.vue.config.contentFormatters.empty; 
         }
-        return numeral(value / 100).format((custom_number_format || '0,0[.]00') + '%');
+        return numeral(value / 100).format((custom_number_format || App.vue.config.contentFormatters.percent) + '%');
       },
       _formatToNumber: function(value, custom_number_format, nullReturn) {
         if (value === undefined || value === null || isNaN(value)) {
-         return nullReturn || '-'; 
+         return nullReturn || App.vue.config.contentFormatters.empty; 
         }
-        return numeral(value).format(custom_number_format || '0,0[.]00');
+        return numeral(value).format(custom_number_format || App.vue.config.contentFormatters.number);
       },
       _formatToPhoneNumber: function(value, reg, nullReturn) {
         if (value === undefined || value === null) {
-         return nullReturn || '-'; 
+         return nullReturn || App.vue.config.contentFormatters.empty; 
         }
         var r = reg || /(?:(\+?(?:(\d{1,2}))\s?)?(\(?(?:(\d{3}))\)?(\s|-|\.)?)((?:(\d{3}))(\s|-|\.)?)((?:(\d{4}))))/i;
         var match = r.exec(value);
@@ -30,14 +40,14 @@
           var areaCode = match[2] || 1;
           return '+' + areaCode + ' (' + match[4] + ') ' + match[7] + '-' + match[10];
         }else{
-          return nullReturn || '-'; 
+          return nullReturn || App.vue.config.contentFormatters.empty; 
         }
       },
       _formatToDate: function(value, custom_format, nullReturn) {
         if (value === undefined || value === null || !moment(value).isValid()) {
-         return nullReturn || '-'; 
+         return nullReturn || App.vue.config.contentFormatters.empty; 
         }
-        return moment(value).format(custom_format || 'MM/DD/YYYY');
+        return moment(value).format(custom_format || App.vue.config.contentFormatters.date);
       }
     }
   };
