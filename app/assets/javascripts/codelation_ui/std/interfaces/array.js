0990298c5776aa9de095f9279039d0067fd003f1
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-07 13:48:53 -0500

    Create array.js

---
+++ app/assets/javascripts/codelation_ui/std/interfaces/array.js
@@ -0,0 +1,28 @@
+(function(){
+  "use strict";
+
+  App.vue.interfaces.array = {
+    methods: {
+      _uniq: function(arr) {
+        if (arr.constructor === Array) {
+          var seen = {};
+          var out = [];
+          var len = arr.length;
+          var j = 0;
+          for(var i = 0; i < len; i++) {
+               var item = arr[i];
+               if(seen[item] !== 1) {
+                     seen[item] = 1;
+                     out[j++] = item;
+               }
+          }
+          return out;
+        }else{
+          console.warn("_uniq only supports arrays");
+          return -1;
+        }
+      }
+    }
+  }
+
+})();
