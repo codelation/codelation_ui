b2ba8e65bb1dd7afac326d65c7f4a6922672437d
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-07 14:41:26 -0500

    Update string.js

--- app/assets/javascripts/codelation_ui/std/interfaces/string.js
+++ app/assets/javascripts/codelation_ui/std/interfaces/string.js
@@ -9,18 +9,6 @@
                      .toLowerCase()
                      .replace((/[ -]/g), '_');
       },
-      // Returns the Plural form of the string
-      _pluralize: function(string) {
-        if (string.substr(string.length - 1) === "s"){
-          return string + "es";
-        }
-
-        if (string.substr(string.length - 1) === "y"){
-          return string.substr(0, string.length - 1) + "ies";
-        }
-
-        return string + "s";
-      },
       // Returns the lowerCamelCase form of a string.
       _dasherize: function(string) {
         return string.replace((/([a-z\d])([A-Z])/g), '$1_$2')
