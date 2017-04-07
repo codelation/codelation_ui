edd120369901a571b26e6eeb77ba134131f7abdc
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-07 14:42:20 -0500

    Update base.js

--- app/assets/javascripts/codelation_ui/base.js
+++ app/assets/javascripts/codelation_ui/base.js
@@ -3,6 +3,7 @@
 //= require turbolinks
 //= require moment
 //= require numeral
+//= require pluralize
 //= require ./_base_resources/app
 //= require ./_base_resources/progress_bar
 //= require_tree ./_base_resources/components
