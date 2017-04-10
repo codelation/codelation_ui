982940a8be1de419499d4ca7921958c1477c1c10
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-10 00:02:20 -0500

    Create fail.js

---
+++ app/assets/javascripts/codelation_ui/extended/fail.js
@@ -0,0 +1,34 @@
+(function() {
+  "use strict";
+
+  App.register('component').enter(function() {
+
+    Vue.component('vue-fail', {
+      template: '<div :style="styles">\
+      <svg class="vue-fail" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">\
+        <circle class="vue-fail-circle" cx="26" cy="26" r="25" fill="none" :stroke-width="stroke"/>\
+        <path class="vue-fail-check" fill="none" d="M16 16 36 36 M36 16 16 36" :stroke-width="stroke"/>\
+      </svg></div>\
+      ',
+      props: {
+        size: {
+          type: Number,
+          default: 50
+        },
+        stroke: {
+          type: Number,
+          default: 3.5
+        }
+      },
+      computed: {
+        styles: function() {
+          var newSize = this.size + 'px';
+          return {
+            width: newSize,
+            height: newSize
+          };
+        }
+      }
+    });
+  });
+})();
