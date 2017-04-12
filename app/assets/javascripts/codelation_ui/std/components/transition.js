697889208d62ee7d954500ed766361bf674282b6
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-12 09:29:17 -0500

    Create transition.js

---
+++ app/assets/javascripts/codelation_ui/std/components/transition.js
@@ -0,0 +1,28 @@
+(function() {
+  "use strict";
+
+  App.register('component').enter(function() {
+
+    var template = '<div v-if="pageTransitionFinished" transition="v-page-load"><slot></slot></div>';
+
+    Vue.component('vue-transition', {
+      props: {
+        transitionName: {
+          type: String,
+          default: "v-fade-up",
+      },
+      template: template,
+      data: function() {
+        return {
+          pageTransitionFinished: false
+        }
+      },
+      ready: function() {
+        var self = this;
+        this.$nextTick(function() {
+          self.pageTransitionFinished = true;
+        });
+      }
+    });
+  });
+})();
