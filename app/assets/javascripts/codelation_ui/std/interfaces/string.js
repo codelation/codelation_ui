17f59801ed52fb89e8bb9d3e3e49b19fae0df323
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-13 11:34:00 -0500

    Update string.js

--- app/assets/javascripts/codelation_ui/std/interfaces/string.js
+++ app/assets/javascripts/codelation_ui/std/interfaces/string.js
@@ -3,18 +3,23 @@
 
   App.vue.interfaces.string = {
     methods: {
-      // Returns the lowerCamelCase form of a string.
+      // Replaces spaces and reverts capital letters to lowercase with underscore before them.
       _underscore: function(string) {
         return string.replace((/([a-z\d])([A-Z])/g), '$1_$2')
                      .toLowerCase()
                      .replace((/[ -]/g), '_');
       },
       _pluralize: pluralize,
-      // Returns the lowerCamelCase form of a string.
+      // Replaces spaces and reverts capital letters to lowercase with dash before them.
       _dasherize: function(string) {
         return string.replace((/([a-z\d])([A-Z])/g), '$1_$2')
                      .toLowerCase()
                      .replace((/[ _]/g), '-');
+      },
+      _titleize: function(string) {
+        return string.replace((/([a-z\d])([A-Z])/g), '$1_$2').toLowerCase().replace(/(?:^|\s|-|_)\S/g, function(m) {
+          return m.toUpperCase();
+        }).replace((/[ _-]/g), ' ');
       }
     }
   }
