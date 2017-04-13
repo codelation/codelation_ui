d5c684311845db8f9d73aa85eece927c6a152858
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-13 11:34:55 -0500

    Update string.js

--- app/assets/javascripts/codelation_ui/std/interfaces/string.js
+++ app/assets/javascripts/codelation_ui/std/interfaces/string.js
@@ -9,6 +9,7 @@
                      .toLowerCase()
                      .replace((/[ -]/g), '_');
       },
+      // Changes the word into the plural form
       _pluralize: pluralize,
       // Replaces spaces and reverts capital letters to lowercase with dash before them.
       _dasherize: function(string) {
@@ -16,6 +17,7 @@
                      .toLowerCase()
                      .replace((/[ _]/g), '-');
       },
+      // Changes a character after any break in the string to the capital version
       _titleize: function(string) {
         return string.replace((/([a-z\d])([A-Z])/g), '$1_$2').toLowerCase().replace(/(?:^|\s|-|_)\S/g, function(m) {
           return m.toUpperCase();
