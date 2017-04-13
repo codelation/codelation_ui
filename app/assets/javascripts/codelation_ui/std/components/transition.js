701bb9efa001a23a21de693e0689119e9ae81a4d
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-13 12:04:43 -0500

    Update transition.js

--- app/assets/javascripts/codelation_ui/std/components/transition.js
+++ app/assets/javascripts/codelation_ui/std/components/transition.js
@@ -1,29 +1,26 @@
 (function() {
   "use strict";
+  
+  var template = '<div v-if="pageTransitionFinished" :transition="transitionName"><slot></slot></div>';
 
-  App.register('component').enter(function() {
-
-    var template = '<div v-if="pageTransitionFinished" :transition="transitionName"><slot></slot></div>';
-
-    Vue.component('vue-transition', {
-      props: {
-        transitionName: {
-          type: String,
-          default: "v-fade-up"
-        }
-      },
-      template: template,
-      data: function() {
-        return {
-          pageTransitionFinished: false
-        }
-      },
-      ready: function() {
-        var self = this;
-        this.$nextTick(function() {
-          self.pageTransitionFinished = true;
-        });
+  App.vue.components.vueTransition = Vue.extend({
+    props: {
+      transitionName: {
+        type: String,
+        default: "v-fade-up"
       }
-    });
+    },
+    template: template,
+    data: function() {
+      return {
+        pageTransitionFinished: false
+      }
+    },
+    ready: function() {
+      var self = this;
+      this.$nextTick(function() {
+        self.pageTransitionFinished = true;
+      });
+    }
   });
 })();
