ece9435524cca193c607b18efefafa3378df6564
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-13 12:03:49 -0500

    Update info.js

--- app/assets/javascripts/codelation_ui/std/components/info.js
+++ app/assets/javascripts/codelation_ui/std/components/info.js
@@ -1,26 +1,22 @@
 (function() {
   "use strict";
-
-  App.register('component').enter(function() {
-
-    var template = '<div class="vue-info" :style="{\'font-size\': size + \'px\'}">\
+  
+  var template = '<div class="vue-info" :style="{\'font-size\': size + \'px\'}">\
                       <i class=\"fa fa-question vue-info-hint\"></i>\
                       <span class="vue-info-message" :class="[direction]">\
                         <slot></slot>\
                       </span>\
                     </div>';
 
-    Vue.component('vue-info', {
-      template: template,
-      props: {
-        'size': {
-          default: '12'
-        },
-        'direction': {
-          default: ''
-        }
+  App.vue.components.vueInfo = Vue.extend({
+    template: template,
+    props: {
+      'size': {
+        default: '12'
+      },
+      'direction': {
+        default: ''
       }
-    });
-
+    }
   });
 })();
