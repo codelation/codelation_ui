18a9b989c37cea381df512706246daaf298db442
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-13 11:45:28 -0500

    Update vue_pages.js

--- app/assets/javascripts/codelation_ui/vue_pages.js
+++ app/assets/javascripts/codelation_ui/vue_pages.js
@@ -2,29 +2,23 @@
   "use strict";
   
   App.register('first').enter(function() {
-    // Insert the customizable global interface
-    if (App.vue.interfaces.custom === undefined) {
-      App.vue.interfaces.custom = {
-        methods: {}
-      };
-    }
     
     // All vue components inherit this.  Used to show the vue page after the js loads
     Vue.mixin({
-      mixins: [App.vue.interfaces.contentFormatters, App.vue.interfaces.string, App.vue.interfaces.custom],
-      methods: {
-        _numeral: numeral,
-        _moment: moment
-      }
+      mixins: App.vue.config.main.includedInterfaces
     });
   });
 
   App.register('last').enter(function() {
-    console.warn("Loaded Vue")
-    console.warn(App.vue);
+    
+    if (App.vue.config.main.showInterfaces) {
+      console.warn("Loaded Vue")
+      console.warn(App.vue);
+    }
+    
     if (App.vue.root === null) {
       App.vue.root = new Vue({
-        el: "body",
+        el: "main",
         components: App.vue.extend,
         data: function() {
           return {
